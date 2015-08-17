4e4992558af5a43e8c52f342a95edafcef8d6e0b
veimis <ilkka.veima@student.tut.fi>
2015-08-17 20:55:25 +0300

    Added comments.

--- controllers/cm_team_controller.js
+++ controllers/cm_team_controller.js
@@ -16,6 +16,10 @@ module.exports = function(pb) {
   util.inherits(TeamController, BaseController);
 
   // Render team template
+  // Render is executed within a domain context and errors thrown 
+  // therefore errors will be handled and result in an error page.
+  // cb = callback(result)
+  // TODO how does call back work for double self.func() calls?
   TeamController.prototype.render = function(cb) {
     var self = this;
     var cos = new pb.CustomObjectService();
