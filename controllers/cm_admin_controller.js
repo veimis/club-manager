88f306539a713a47aee4237743ca6aea31a4543c
veimis <veimis@something.lost>
2015-08-21 18:05:33 +0300

    Added subnavigation for club manager settings.

--- controllers/cm_admin_controller.js
+++ controllers/cm_admin_controller.js
@@ -11,7 +11,10 @@ module.exports = function(pb) {
   // Inherits from base controller: accessors for template service, 
   // localization service, request and response handlers.
   util.inherits(AdminController, BaseController);
-
+  
+  // Subnavigation key
+  var SUB_NAV_KEY = 'club_manager_index';
+  
   // Render admin page
   // Render is executed within a domain context and errors thrown 
   // therefore errors will be handled and result in an error page.
@@ -23,6 +26,7 @@ module.exports = function(pb) {
     self.ts.registerLocal('angular', function(flag, cb) {
       var objects = {
         navigation: pb.AdminNavigation.get(self.session, ['dashboard'], self.localizationService), 
+        pills: pb.AdminSubnavService.get(SUB_NAV_KEY, self.ls),
         access: self.session.authentication.admin_level,
         tabs: self.getTabs()
       };
@@ -69,5 +73,20 @@ module.exports = function(pb) {
       }
     ];
   };
+
+  AdminController.getSubNavItems = function(key, ls, data) {
+    return [
+      {
+        name: 'manage_club',
+        title: 'Manage club',
+        icon: 'refresh',
+        href: '/club-manager/admin'
+      }
+    ];
+  };
+
+  // Register club manager admin subnavigation
+  pb.AdminSubnavService.registerFor(SUB_NAV_KEY, AdminController.getSubNavItems);
+ 
   return AdminController;
 };
