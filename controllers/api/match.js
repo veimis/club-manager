05fb5d9b983606f8a2c0504559aba7db58402e0f
Ilkka Veima <ilkka.veima@gmail.com>
2016-04-09 15:41:26 +0300

    Bare bones match api controller and post request.

--- controllers/api/match.js
+++ controllers/api/match.js
@@ -7,5 +7,24 @@ module.exports = function(pb) {
   function MatchApiController(){}
   util.inherits(MatchApiController, pb.BaseApiController);
 
+  // Register controller routes 
+  MatchApiController.getRoutes = function(cb) {
+    const routes = [{
+      method: "post",
+      path: "/club-manager/api/stats",
+      auth_required: true,
+      content_type: 'application/json',
+      request_body: ['application/json', 'application/x-www-form-urlencoded', 'multipart/form-data'],
+      handler: 'saveStats'
+    }];
+
+    cb(null, routes);
+  };
+
+  // Save new statistics
+  MatchApiController.prototype.saveStats = function(cb) {
+    console.log(this.body);
+  };
+
   return MatchApiController;
 }
