6734db4ab83cf72b908117a313e2e9abf6727d0f
Ilkka Veima <ilkka.veima@gmail.com>
2016-04-23 17:55:58 +0300

    Add test for team module.

---
+++ test/teamSpec.js
@@ -0,0 +1,14 @@
+// Tests for team module
+
+var expect = require('chai').expect;
+
+describe('Team', function(){
+  describe('#require', function(){
+    it('Should have the correct properties', function() {
+      var player = require('../lib/team.js');
+      expect(player).to.have.a.property('install');
+      expect(player).to.have.a.property('uninstall');
+      expect(player).to.have.a.property('getAll');
+    });
+  });
+});
